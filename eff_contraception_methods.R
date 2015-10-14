5d2c649960c1be07d811df085b6a6566026a195f
tdawry <travisdawry@gmail.com>
2015-10-14 00:13:49 -0400

    added plot.ly integration and cleaned up comments

--- eff_contraception_methods.R
+++ eff_contraception_methods.R
@@ -27,12 +27,12 @@ implanon <- 0.0005
 female.ster <- 0.005
 male.ster <- 0.0015
 
-#prob of getting 1 or greater failures in N years with given prob per method X
+# prob of getting 1 or greater failures in N years with given prob per method X
 FailRate <- function(years, method){
      pbinom(0, years, method, lower.tail=FALSE)
 }
 
-#plotted prob of getting 1 or greater failures in N years with given prob per method X
+# plotted prob of getting 1 or greater failures in N years with given prob per method X
 PlotFailRate <- function(years, method){
      rate <<- pbinom(0, 1:years, method, lower.tail=FALSE)
      p <- ggplot(as.data.frame(rate), aes(x = seq(1:length(rate)), y = rate))
@@ -42,7 +42,7 @@ PlotFailRate <- function(years, method){
      scale_colour_gradient(high = "red")
 }
 
-#example input/output
+# example input/output
 FailRate(4, condom.male)
 
 FailRate(4, condom.male * pill)
@@ -51,7 +51,7 @@ PlotFailRate(4, condom.male)
 
 PlotFailRate(4, condom.male * pill)
 
-#create data.frame of all methods in order to plot comparison of long term results
+# create data.frame of all methods in order to plot comparison of long term results
 all.methods <- data.frame(condom.female, condom.male, depoprovera, diaphram, female.ster,
                 fertility.aware, implanon, iuc.mirena, iuc.paragard, male.ster,
                 no.method, nuvaring, pill, spermicide, sponge.n.parous,
@@ -59,8 +59,8 @@ all.methods <- data.frame(condom.female, condom.male, depoprovera, diaphram, fem
 
 all.methods <- all.methods[, order(all.methods)]
 
-#create data.frame to include upper tail pbinom of each method over lifetime
-#fertility years
+# create data.frame to include upper tail pbinom of each method over lifetime
+# fertility years
 lifetime.failure <- as.data.frame(matrix(NA, nrow = 20, ncol = 18))
 
 lifetime.failure[18] <- 1:20
@@ -68,20 +68,20 @@ lifetime.failure[18] <- 1:20
 names(lifetime.failure) <- names(all.methods)
 names(lifetime.failure)[18] <- "years"
 
-#would prefer to do this without looping, but looped for readability
+# would prefer to do this without looping, but looped for readability
 for (n in 1:17){
   lifetime.failure[, n] <- pbinom(0, 1:20, as.numeric(all.methods[n]), lower.tail=FALSE)
 }
 
-#inverse of lifetime.failure is taken as lifetime.success, representing the
-#chance of initial pregnancy after N years
+# inverse of lifetime.failure is taken as lifetime.success, representing the
+# chance of initial pregnancy after N years
 lifetime.success <- 1 - lifetime.failure
 lifetime.success[18] <- lifetime.failure[18]
 
-#create plot of lifetime.failure by first melting dataframe and then drawing
+# create plot of lifetime.failure by first melting dataframe and then drawing
 melted.success <- melt(lifetime.success, id = "years", variable_name = "method")
 
-#draw using cairo for anti-aliasing
+# draw using cairo for anti-aliasing
 png(height=810, width=1440, filename="eff_contraception_methods.png", type="cairo")
 
 p <- ggplot(melted.success, aes(years, value)) +
@@ -93,9 +93,12 @@ p <- ggplot(melted.success, aes(years, value)) +
        x = "Years of Continuous Use",
        color = "Method, By Effectiveness")
 
-#print plot to file
+# print plot to file
 p
 dev.off()
 
 ########## Only 14 lines show up on plot, but 17 methods due to nuvaring/pill,
 ########## diaphram/sponge.n.parous, fertility.aware/sponge.parous equivalence
+
+# plot.ly integration; export to plot.ly
+gg <- ggplotly(p)
